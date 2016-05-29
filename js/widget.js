eab3c3cdbcac8cd7f68ceb694bf8200483dbc7ab
Crystole <crystole.roberts@gmail.com>
2016-05-28 22:50:04 -0400

    TH AJAX Basics Employee and Rooms Intranet

---
+++ js/widget.js
@@ -0,0 +1,41 @@
+var xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function () {
+  if(xhr.readyState === 4 && xhr.status === 200) {
+    var employees = JSON.parse(xhr.responseText);
+    var statusHTML = '<ul class="bulleted">';
+    for (var i=0; i<employees.length; i += 1) {
+    if (employees[i].inoffice === true) {
+        statusHTML += '<li class="in">';
+      } else {
+        statusHTML += '<li class="out">';
+      }
+      statusHTML += employees[i].name;
+      statusHTML += '</li>';
+    }
+    statusHTML += '</ul>';
+    document.getElementById('employeeList').innerHTML = statusHTML;
+  }
+};
+xhr.open('GET', '../data/employees.json');
+xhr.send();
+
+var abc = new XMLHttpRequest();
+abc.onreadystatechange = function () {
+  if(abc.readyState === 4 && abc.status === 200) {
+    var rooms = JSON.parse(abc.responseText);
+    var statusHTML = '<ul class="rooms">';
+    for (var i=0; i<rooms.length; i += 1) {
+      if (rooms[i].available === true) {
+        statusHTML += '<li class="empty">';
+      } else {
+        statusHTML += '<li class="full">';
+      }
+      statusHTML += rooms[i].room;
+      statusHTML += '</li>';
+    }
+    statusHTML += '</ul>';
+    document.getElementById('roomList').innerHTML = statusHTML;
+  }
+};
+abc.open('GET', '../data/rooms.json');
+abc.send();
